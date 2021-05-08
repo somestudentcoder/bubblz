73369c12b6d47cf4c337ce035d86d9f15c735f13
somestudentcoder <christopher.oser@student.tugraz.at>
2021-05-08 13:37:11 +0200

    some cleanup

--- src/scripts/bubble.ts
+++ src/scripts/bubble.ts
@@ -1,8 +1,9 @@
 
 
-export class bubble{
+export class Bubble{
     public x_position: number = 0;
     public y_position: number = 0;
+    public children: Bubble[] = [];
 
     constructor(){
         
