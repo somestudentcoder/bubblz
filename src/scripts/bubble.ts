17daa67d2d677e7a18721a2190df5808cb87abb0
Markus Ruplitsch <markus.ruplitsch@student.tugraz.at>
2021-05-08 15:05:20 +0200

    minor change in initial bubble positions

--- src/scripts/bubble.ts
+++ src/scripts/bubble.ts
@@ -17,14 +17,15 @@ export class Bubble{
 
     static from(node: HierarchyNode<any>, parent?: Bubble){
         let bubble = new Bubble();
+        bubble.id = model.getNewID();
         bubble.body = new p2.Body({
             mass:5,
-            position:[100,1000]
+            position:[100 + bubble.id,1000]
         });
         bubble.body.addShape(new p2.Circle({ radius: 1 }));
         bubble.radius = Math.sqrt(node.data.weight / Math.PI);
         bubble.name = node.data.name;
-        bubble.id = model.getNewID();
+        
         if(node.children != undefined){
             for(let child of node.children){
                 bubble.children.push(Bubble.from(child));
